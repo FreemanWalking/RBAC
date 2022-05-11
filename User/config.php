bdbe8619e6bcb3354d877ae1370e40639aa516f9
GoldiaV <yunminsu19@gmail.com>
2022-05-11 18:02:55 +0700

    ClearDB database

--- User/config.php
+++ User/config.php
@@ -1,9 +1,9 @@
 <?php 
 
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "rbac";
+$server = "us-cdbr-east-05.cleardb.net";
+$user = "b95134a8b31d93";
+$pass = "2c157723";
+$database = "heroku_89873beb75ba2c3";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 
